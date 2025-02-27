3a1250be92792cf86b782dd5178bedb82a8a6941
An Pham <vnswann@gmail.com>
2025-02-27 23:14:13 +0700

    update crud company
    query filter

--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -1,12 +1,17 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.domain.dto.Meta;
+import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.repository.UserRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,20 +23,36 @@ public class UserService {
     }
 
     public User handleCreateUser(User user) {
-       return this.userRepository.save(user);
+        return this.userRepository.save(user);
     }
 
-    public void handleDeleteUser(Long id ) {
+    public void handleDeleteUser(long id) {
         this.userRepository.deleteById(id);
     }
 
-    public User fetchUserById(Long id) {
-        Optional<User> user = this.userRepository.findById(id);
-        return user.orElse(null);
+    public User fetchUserById(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
     }
 
-    public List<User> fetchAllUser(){
-        return this.userRepository.findAll();
+    public ResultPaginationDTO fetchAllUser(Specification<User> spec, Pageable pageable) {
+        Page<User> pageUser = this.userRepository.findAll(spec, pageable);
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        Meta mt = new Meta();
+
+        mt.setPage(pageable.getPageNumber() + 1);
+        mt.setPageSize(pageable.getPageSize());
+
+        mt.setPages(pageUser.getTotalPages());
+        mt.setTotal(pageUser.getTotalElements());
+
+        rs.setMeta(mt);
+        rs.setResult(pageUser.getContent());
+
+        return rs;
     }
 
     public User handleUpdateUser(User reqUser) {
@@ -40,9 +61,8 @@ public class UserService {
             currentUser.setEmail(reqUser.getEmail());
             currentUser.setName(reqUser.getName());
             currentUser.setPassword(reqUser.getPassword());
-            //update
+            // update
             currentUser = this.userRepository.save(currentUser);
-
         }
         return currentUser;
     }
